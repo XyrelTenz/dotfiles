3e9a992c2e11b6fd1c7986180269189db27e9f0a
XyrelTenz <xdemocrito1@gmail.com>
2025-11-24 22:52:58 +0800

    config colors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,8 @@ vim.opt.shellquote = ""
 vim.opt.shellpipe = "|"
 vim.opt.shellredir = ">"
 vim.opt.shellxquote = ""
+
+-- Colors
+vim.lsp.document_color_default_options = {
+  mode = "background",
+}
