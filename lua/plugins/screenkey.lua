206725ff10fa455f9f4cb51c0d62f047838b2cb8
XyrelTenz <xdemocrito1@gmail.com>
2025-11-29 19:16:56 +0800

    Config Minor

--- lua/plugins/screenkey.lua
+++ lua/plugins/screenkey.lua
@@ -4,7 +4,15 @@ return {
   version = "*",
   config = function()
     require("screenkey").setup({
-      -- win_opts = { row = 2, col = 2 },
+      win_opts = {
+        relative = "editor",
+        anchor = "SE", -- Anchor the window's South-East corner...
+        width = 25,
+        height = 1,
+        border = "rounded",
+        row = vim.o.lines - vim.o.cmdheight - 1,
+        col = vim.o.columns - 1,
+      },
     })
 
     vim.schedule(function()
