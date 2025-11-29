143284073e3f238b0cb392238f0ebfc1972b36f2
XyrelTenz <xdemocrito1@gmail.com>
2025-11-29 19:24:34 +0800

    Floating Neo Tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "float",
+      popup = {
+        size = { height = "80%", width = "80%" },
+        position = "50%",
+      },
+    },
+  },
+}
