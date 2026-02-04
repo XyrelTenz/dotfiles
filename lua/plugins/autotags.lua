aa81d5eaf2733d484abfadae300ebd9f6255e78e
XyrelTenz <xdemocrito1@gmail.com>
2026-02-04 10:59:13 +0800

    added

--- lua/plugins/autotags.lua
+++ lua/plugins/autotags.lua
@@ -1,6 +1,6 @@
 return {
   "windwp/nvim-ts-autotag",
-  ft = { "html", "javascriptreact", "typescriptreact", "xml", "vue", "svelte" },
+  ft = { "html", "javascriptreact", "typescriptreact", "xml", "vue", "svelte", "astro" },
   config = function()
     require("nvim-ts-autotag").setup({
       opts = {
